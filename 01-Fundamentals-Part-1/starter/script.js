93de5c4026930765c31f292effdbcacb2b91716a
Nisha <nishafarah24.2@gmail.com>
2023-09-02 21:29:19 +0700

    Variabel pada JS

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+let js = 'amazing';
+// if (js === 'amazing') alert ("JavaScript is FUN!");
+
+console.log(12+60/5);
+
+let firstName = 'Nisha';
+console.log(firstName);
+
+let firstname = 'nisha';
+console.log(firstname);
+
+// penggunaannya untuk di OOP
+let Lastname = 'Farah';
+console.log(Lastname);
+
+let last_name = 'Fadilla';
+console.log(last_name);
+
+let number8 = 8;
+console.log(number8);
+
+// penggunaan yang salah:
+// let 8number = 18;
+// console.log(8number);
+
+let dollar$ = 'money';
+console.log(dollar$);
+
+let $dollar = 'moneys';
+console.log($dollar);
+
+// pastikan nama variabel (let) bukan 'Reserved Word'
